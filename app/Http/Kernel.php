1afc64e6a76eaa1fc206e777ac974225320d7f33
HiiiSham <HiiiSham@DESKTOP-AIOTIL8>
2025-08-09 15:38:12 +0200

    FINISHED

---
+++ app/Http/Kernel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array<int, class-string|string>
+     */
+    protected $middleware = [
+        // Autres middlewares...
+        \App\Http\Middleware\Cors::class,
+    ];
+
+    // ...
+}
